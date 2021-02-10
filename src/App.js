a537cfd9a93552684c3fc8fa0d37282f112e1855
Meet Patel <meetaiims@yahoo.com>
2021-02-09 18:44:25 -0600

    add rudimentary navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import React from 'react'
 export default function App() {
   return (
     <>
-      Hello Portfolio!
+      <header>
+        <h1>Meet Patel</h1>
+        <nav>
+          <ul>
+            <li>Intro</li>
+            <li>About</li>
+            <li>TechStack</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
     </>
-  )
+  );
 }
