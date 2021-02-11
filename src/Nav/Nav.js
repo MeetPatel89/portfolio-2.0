be43ab9f0ac23edb92472cdd0c6d565983ad2551
Meet Patel <meetaiims@yahoo.com>
2021-02-11 03:50:46 -0600

    responsive navbar-menu pops up onClick hamburger

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nav.css';
 
-export default function Nav() {
-  return (
-    <nav className='navbar'>
-      <div className='name'>Meet Patel</div>
-      <div className='toggle-button-div'>
-        <a href='#' class='toggle-button'>
-          <span class='bar'></span>
-          <span class='bar'></span>
-          <span class='bar'></span>
-        </a>
-      </div>
+export default class Nav extends Component {
 
-      <div className='navbar-links'>
-        <ul>
-          <li>
-            <a href='#'>Home</a>
-          </li>
-          <li>
-            <a href='#'>About</a>
-          </li>
-          <li>
-            <a href='#'>TechStack</a>
-          </li>
-          <li>
-            <a href='#'>Projects</a>
-          </li>
-          <li>
-            <a href='#'>Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      navLinks: 'hidden'
+    }
+  }
+
+  handleclick = () => {
+   this.setState({
+     navLinks: !this.state.navLinks ? 'hidden' : ''
+   })
+  }
+
+  render() {
+      return (
+        <nav className='navbar'>
+          <div className='name'>Meet Patel</div>
+          <div className='toggle-button-div'>
+            <a href='#' class='toggle-button' onClick={this.handleclick}>
+              <span class='bar'></span>
+              <span class='bar'></span>
+              <span class='bar'></span>
+            </a>
+          </div>
+
+          <div className={`navbar-links ${this.state.navLinks}`}>
+            <ul>
+              <li>
+                <a href='#'>Home</a>
+              </li>
+              <li>
+                <a href='#'>About</a>
+              </li>
+              <li>
+                <a href='#'>TechStack</a>
+              </li>
+              <li>
+                <a href='#'>Projects</a>
+              </li>
+              <li>
+                <a href='#'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+  }
+  
 }
